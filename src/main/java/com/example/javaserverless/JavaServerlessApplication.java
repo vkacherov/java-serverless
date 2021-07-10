4408258af99584463521b40f62ad6db7578d0afb
Vadim Kacherov <vkacherov@google.com>
2021-07-09 21:07:15 -0400

    Init checkin

---
+++ src/main/java/com/example/javaserverless/JavaServerlessApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaserverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServerlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerlessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaserverless/JavaServerlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaserverless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServerlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
